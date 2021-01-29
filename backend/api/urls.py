e25f42ff7672c3bf7e5683c40852dc035b567190
2400908v <2400908v@student.gla.ac.uk>
2021-01-29 13:29:50 +0000

    Added modal for updating profile

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from api.views import BlacklistTokenUpdateView, ProjectAPIView, UserRegisterView, LandmarkAPIView
+from api.views import BlacklistTokenUpdateView, ProjectAPIView, UserRegisterView, LandmarkAPIView, UserDetailsAPIView
 from api.views_ajax import check_email, check_username
 
 router = DefaultRouter()
 router.register('landmarks', LandmarkAPIView, basename='landmarks')
 router.register('projects', ProjectAPIView, basename='projects')
+router.register('user-details', UserDetailsAPIView, basename='user-details')
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
--- frontend/psd-prototype/src/components/UserGroupsCard.js
+++ frontend/psd-prototype/src/components/UserGroupsCard.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -34,7 +32,6 @@ export default function UserGroupsCard(props) {
     <React.Fragment>
         <div className={classes.paper}>
             <Card className={classes.root}>
-                <CardActionArea>
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="h2">
                     My Groups
@@ -58,7 +55,6 @@ export default function UserGroupsCard(props) {
                         </Table>
                     </TableContainer>
                 </CardContent>
-                </CardActionArea>
                 <CardActions>
                 <Button size="small" color="primary">
                     Create a new group
