f936a0e6392a138eac73ddfb1707de8440f093ab
Alex <2382854p@student.gla.ac.uk>
2021-02-10 12:16:25 +0000

    integrated basic rte that allows for advanced marker editing, still to allow images

--- frontend/psd-prototype/src/components/EditMarker.js
+++ frontend/psd-prototype/src/components/EditMarker.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component, PropTypes} from "react";
 import { Button, MenuItem, InputLabel, Select } from '@material-ui/core/';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
@@ -8,13 +8,19 @@ import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
+import RichTextEditor from 'react-rte'
 
 export default class EditMarker extends React.Component{
+
+    /*static propTypes = {
+        onChange: PropTypes.func
+    }*/
 
     state = {
         content: this.props.content,
         icontype: this.props.icontype,
-
+        //value: RichTextEditor.createEmptyValue()
+        value: RichTextEditor.createValueFromString(this.props.content, 'html'),
     }
 
     handleEdit = () => {
@@ -30,22 +36,36 @@ export default class EditMarker extends React.Component{
         this.setState({icontype: event.target.value})
     }
 
+    onChange = (value) => {
+        console.log('on change');
+        this.setState({value})
+        this.setState({content: value.toString('html')})
+        //this.setState({content: e.toString('html')});
+        if (this.props.onChange) {
+            console.log('dab');
+            this.props.onChange(
+               value.toString('html')
+            );
+        }
+    };
+
     render() {
         return(
             <React.Fragment>
                 <Grid container spacing={2} direction="column">
                     <Grid item>
-                        {this.props.content}
+                        <div dangerouslySetInnerHTML={{__html: this.props.content}}></div>
                     </Grid>
                     {/* <Grid item>
                         {"icon type = " + this.props.icontype}
                     </Grid> */}
                     <Grid item>
                         <InputLabel id="label">Content</InputLabel>
-                        <TextField
-                            value = {this.state.content}
-                            onChange = {e => this.setState({content: e.target.value})} 
-                            />
+                        <RichTextEditor
+                            value = {this.state.value}
+                            onChange = {this.onChange}
+                            //onChange = {e => this.setState({content: e.target.value})} 
+                        />
                     </Grid>
                     <Grid item>
                         <InputLabel id="label">Icon type</InputLabel>
