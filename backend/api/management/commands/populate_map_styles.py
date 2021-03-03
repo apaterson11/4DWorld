cddc1af4318a8b33c866275e494dc6a662e649b2
2386278f <2386278f@student.gla.ac.uk>
2021-03-03 11:28:46 +0000

    limited map option to 10

--- backend/api/management/commands/populate_map_styles.py
+++ backend/api/management/commands/populate_map_styles.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
 
         # Map style options; reference: https://leaflet-extras.github.io/leaflet-providers/preview/
         MAP_OPTIONS = [
-            {
+               {
                 'name': 'OpenStreetMap: Mapnik (Default)',
                 'url': 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
                 'min_zoom': 1,
@@ -32,13 +32,6 @@ class Command(BaseCommand):
                 'attribution': 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
             },
             {
-                'name': 'Stadia: AlidadeSmooth',
-                'url': 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-            },
-            {
                 'name': 'Stadia: AlidadeSmoothDark',
                 'url': 'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png',
                 'min_zoom': 1,
@@ -46,81 +39,11 @@ class Command(BaseCommand):
                 'attribution': '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
             },
             {
-                'name': 'Stadia: OSMBright',
-                'url': 'https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-            },
-            {
-                'name': 'Stadia: Outdoors', 
-                'url': 'https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-            },
-            {
-                'name': 'CyclOSM',
-                'url': 'https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': '<a href="https://github.com/cyclosm/cyclosm-cartocss-style/releases" title="CyclOSM - Open Bicycle render">CyclOSM</a> | Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            },
-            {
-                'name': 'Stamen: Toner',
-                'url': 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            },
-            {
-                'name': 'Stamen: TonerBackground',
-                'url': 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            },
-            {
-                'name': 'Stamen: TonerLite', 
-                'url': 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            },
-            {
-                'name': 'Stamen: Watercolor',
-                'url': 'https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png',
-                'min_zoom': 1,
-                'max_zoom': 16,
-                'attribution': 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            },
-            {
                 'name': 'Stamen: Terrain',
                 'url': 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.png',
                 'min_zoom': 1,
                 'max_zoom': 16,
                 'attribution': 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            },
-            {
-                'name': 'Esri: WorldStreetMap',
-                'url': 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
-            },
-            {
-                'name': 'Esri: DeLorme',
-                'url': 'https://server.arcgisonline.com/ArcGIS/rest/services/Specialty/DeLorme_World_Base_Map/MapServer/tile/{z}/{y}/{x}',
-                'min_zoom': 1,
-                'max_zoom': 11,
-                'attribution': 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
-            },
-            {
-                'name': 'Esri: WorldTopoMap',
-                'url': 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
             },
             {
                 'name': 'Esri: WorldImagery',
@@ -144,22 +67,8 @@ class Command(BaseCommand):
                 'attribution': 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC'
             },
             {
-                'name': 'Esri: WorldGrayCanvas',
-                'url': 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
-                'min_zoom': 1,
-                'max_zoom': 16,
-                'attribution': 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
-            },
-            {
                 'name': 'CartoDB: Positron',
                 'url': 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            },
-            {
-                'name': 'CartoDB: PositronNoLabels',
-                'url': 'https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png',
                 'min_zoom': 1,
                 'max_zoom': 19,
                 'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
@@ -170,41 +79,6 @@ class Command(BaseCommand):
                 'min_zoom': 1,
                 'max_zoom': 19,
                 'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            },
-            {
-                'name': 'CartoDB: DarkMatterNoLabels', 
-                'url': 'https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            },
-            {
-                'name': 'CartoDB: Voyager',
-                'url': 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            },
-            {
-                'name': 'CartoDB: VoyagerNoLabels', 
-                'url': 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}{r}.png',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            },
-            {
-                'name': 'CartoDB: VoyagerLabelsUnder',
-                'url': 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            },
-            {
-                'name': 'HikeBike: HikeBike',
-                'url': 'https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png',
-                'min_zoom': 1,
-                'max_zoom': 19,
-                'attribution': '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }
         ]
 
