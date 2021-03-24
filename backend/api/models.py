979d8c1b4d91b732ea0a0f57ca02ff9053681d25
2400908v <2400908v@student.gla.ac.uk>
2021-03-24 19:11:46 +0000

    Merging EditMarker to work with both protomap and editmap

--- backend/api/models.py
+++ backend/api/models.py
@@ -102,7 +102,7 @@ class LandmarkImage(models.Model):
     landmark = models.ForeignKey(Landmark, on_delete=models.CASCADE,
                                  related_name="landmark", parent_link=True, null=False, default=None)
     image = models.ImageField(max_length=1000, upload_to='images/', null=False, default=None)
-    image_name = models.CharField(default="", max_length=64)
+    image_name = models.CharField(default="", max_length=1000)
 
     def __str__(self):
         return str(self.landmark.id)
