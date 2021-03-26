1f58bf88622f7e262dba26c809bb5e085804e8fd
Alex <2382854p@student.gla.ac.uk>
2021-03-26 22:35:34 +0000

    population script - passwords don't work

--- backend/populate.py
+++ backend/populate.py
@@ -1,17 +1,40 @@
 import os
+import sys
+import csv
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'psd_backend.settings')
 
 import django
 django.setup()
-from api.models import Layer
+from api.models import User
 
 def main():
-    add_layer("Default", "Default")
+    cwd = os.getcwd()
+    for root, dirs, files in os.walk(cwd):
+        for file in files:
+            if file.endswith(".csv"):
+                f=open(file, 'r')
+                rows = csv.reader(f)
+                next(rows, None)
+                    
+                for row in rows:
+                    add_user(row[2], row[2].split('@')[0], row[3], row[0], row[1], row[4], row[5])
+                f.close()
 
-def add_layer(name, description):
-    l = Layer.objects.get_or_create(name=name, description=description)[0]
-    l.save()
-    return l
+def add_user(email, username, password, first_name, last_name, superuser, status):
+    u = User.objects.get_or_create(username=username, email=email, password=password, first_name=first_name, last_name=last_name)[0]
+    if (superuser == "Manager"):
+        u.is_superuser = True
+        u.is_staff = True
+    else:
+        u.is_staff = False
+        u.is_superuser = False
+    
+    if (status == "Suspended"):
+        u.is_active = False
+    else:
+        u.is_active = True
+    print("Added user: " + email)
+    u.save()
 
 if __name__ == "__main__":
     main()
